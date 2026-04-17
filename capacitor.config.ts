6e9e9cf806fc53bc79dd4741ac136433b5a7e306
Your Name <you@example.com>
2026-04-17 17:19:16 -0300

    primer commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cashflow_front',
+  webDir: 'www'
+};
+
+export default config;
